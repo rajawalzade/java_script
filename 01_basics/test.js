0c4ad77341e001a826c8bdc34b61c750c210e64c
Rajat Walzade <ext.rajat.walzade@cargotec.com>
2024-02-09 20:19:32 +0530

    Demo Branch

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,28 @@
 console.log("Rajat")
 
+"use strict";// treat all JS as newer version
+
+/* alert(3+3) // we are using nodejs, not browser*/
+
+console.log(3
+    +
+    3)// code readability should be high
+console.log("Rajat")
+
+let name = "Rajat"
+let age = 3
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint 
+// String => ""
+// boolean => true/false
+// null = standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof null);      // out put -object 
+console.log(typeof undefined);  // out put -undefined
